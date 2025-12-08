18bdd4c2aee52e05d5d2facb5c2406d9704bcfe2
panteLx <info@pantelx.com>
2025-12-08 20:50:41 +0100

    feat: Implement themed toaster component for better user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getLocale } from "next-intl/server";
-import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ThemedToaster } from "@/components/themed-toaster";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -50,17 +50,7 @@ export default async function RootLayout({
         >
           <NextIntlClientProvider messages={messages} locale={locale}>
             {children}
-            <Toaster
-              position="bottom-left"
-              richColors
-              closeButton
-              toastOptions={{
-                className: "text-sm",
-                style: {
-                  padding: "12px 16px",
-                },
-              }}
-            />
+            <ThemedToaster />
           </NextIntlClientProvider>
         </ThemeProvider>
       </body>
